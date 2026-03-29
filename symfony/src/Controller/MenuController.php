ce2bce634384d4e1b2fe0273dff5f457a052c9f1
Lucas CAPPELLO <cappellolucas@icloud.com>
2026-03-29 01:40:26 +0100

    feat(menu): add menu detail with image in menus list

--- symfony/src/Controller/MenuController.php
+++ symfony/src/Controller/MenuController.php
@@ -1,6 +1,5 @@
 <?php
 
-// Contrôleur lié à la table (Entité) Menu
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,17 +7,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\MenuRepository;
 
+// Contrôleur lié à l'Entité Menu
 final class MenuController extends AbstractController
 {
     #[Route('/menu', name: 'app_menu')]
     public function index(MenuRepository $menuRepository): Response
     {
-        // findAll() est une méthode utiliser par le fichier MenuRepository qui permet de récupérer les données de l'Entité Menu
+        // findAll() est une méthode de MenuRepository qui permet de récupérer toutes les données de l'Entité Menu
         $menus = $menuRepository->findAll();
 
         return $this->render('menu/index.html.twig', [
-            'controller_name' => 'MenuController',
             'menus' => $menus,
+        ]);
+    }
+
+    // Route pour l'affichage des détails d'un menu
+    #[Route('/menu/{id}', name: 'app_menu_detail')]
+    public function show(int $id, MenuRepository $menuRepository): Response
+    {
+        // find() est une méthode de MenuRepository qui permet de récupérer une seule donnée de l'Entité Menu
+        $menu = $menuRepository->find($id);
+
+        // Exception en cas de problème
+        if(!$menu) {
+            throw $this->createNotFoundException('Menu non trouvé ! Veuillez ressayer ou sélectionner un autre menu');
+        }
+
+        return $this->render('menu/detail.html.twig', [
+            'menu' => $menu,
         ]);
     }
 }
