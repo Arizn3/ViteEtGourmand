58141b5410bc19934e5e5f3fc4ca3b25d35dc414
Lucas CAPPELLO <cappellolucas@icloud.com>
2026-04-20 17:15:53 +0200

    feat(menu): add a confirmation before deleting a menu

---
+++ symfony/public/javascript/employe-menu.js
@@ -0,0 +1,17 @@
+// DOMContentLoaded permet de charger le JS uniquement et strictement quand le HTML est prêt
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.querySelectorAll('.btn-supression').forEach(
+        function (bouton) {
+            bouton.addEventListener('click', function (e) {
+                const confirmation = confirm('Supprimer le menu ?');
+
+                if (!confirmation) {
+                    // Bloque la requête de suppression
+                    e.preventDefault();
+                };
+            });
+        }
+    );
+
+});
