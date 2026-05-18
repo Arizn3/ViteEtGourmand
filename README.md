f2414a6b4936bc7350aeb5f14bd864b2b2070d94
Lucas CAPPELLO <cappellolucas@icloud.com>
2026-05-18 20:30:13 +0200

    docs(readme.md): add project README and demo SQL dataset

---
+++ README.md
@@ -0,0 +1,143 @@
+# Vite & Gourmand
+## Présentation
+
+Vite & Gourmand est une application web de restauration et de livraison permettant aux utilisateurs de consulter des menus, et une fois inscrit de passer des commandes et gérer leurs livraisons en ligne.
+L’application intègre différents rôles du simple utilisateur au rôle employé et administrateur qui assure la gestion des menus, des commandes, des livraisons et du suivi global de l’activité.
+
+## Fonctionnalités principales
+
+- Inscprition, authentification et gestion automatique des accès par rôles via Symfony
+- CRUD des menus par les rôles employé et administrateur
+- Système de commandes disponible uniquement en Gironde
+- Gestion du statut des commandes et des avis utilisateurs par les rôles employé et administrateur
+- Tableau de bord administrateur avec statistiques et création de comptes employés
+
+## Fonctionnalités secondaires
+
+- Système d’envoi automatique d’e-mails lors d’une inscription ou d’un changement de statut d’une commande, etc.
+- Réinitialisation sécurisée du mot de passe
+- Prise de contact rapide avec l’administrateur via un formulaire de contact
+- Système d’avis utilisateurs limité et contrôlé
+
+## Technologies utilisées
+### Backend
+- PHP 8.3-fpm-alpine
+- Symfony 7.4.7
+
+### Frontend
+- HTML
+- CSS
+- JavaScript
+- Twig
+
+### Base de données
+- MySQL 8.0
+- MongoDB 7.0 (La BDD MongoDB est utiliser exclusivement pour les statistiques disponible dans l'application)
+
+### Outils & services
+
+- Docker 4.73.0 (conteneurisation de l’application)
+- Nginx 1.26-alpine (serveur web)
+- OpenStreetMap API (sélection et validation des villes de livraison en Gironde)
+- Chart.js (affichage des statistiques administrateur)
+
+## Méthodes de traitement des données
+
+- Soft Delete via `deleted_at`
+- Traçabilité des données via `created_at`
+
+Mécanismes mis en place afin d’assurer une meilleure traçabilité des données et de respecter les bonnes pratiques de gestion et de conservation des informations.
+
+## Guide d'installation rapide
+### Prérequis
+> [!IMPORTANT]
+> Avant de lancer le projet, assurez-vous d’avoir installé les outils suivants :
+
+- Docker
+- Docker Compose
+- Git
+
+
+Le projet utilise une architecture conteneurisée avec Docker afin de simplifier l’installation et le lancement des différents services comme les bases de données.<br>
+Aucune installation locale de PHP, MySQL ou Symfony n’est nécessaire. Tous les services sont exécutés via Docker.
+
+### Installation
+
+Une base de données MySQL préconfigurée est automatiquement importée lors du premier démarrage des conteneurs.<br>
+Celle-ci contient un compte administrateur de démonstration permettant un accès immédiat à l’application.<br>
+Ce compte Administrateur est configurable par la suite, les identifiants sont disponibles dans la documentation.
+
+### Installation Demo *(Par défaut)*
+
+> [!TIP]
+> L’installation Demo est utilisée par défaut afin de faciliter les tests et la démonstration du projet.
+
+Après avoir cloné le dépôt, lancer la construction des conteneurs :
+
+```bash
+docker compose up -d
+```
+
+Récupérer les données démo MongoDB pour le graphique administrateur :<br>
+
+```bash
+docker exec -i app-mongodb-1 mongorestore \
+-u symfony \
+-p secret \
+--authenticationDatabase admin \
+--db vitegourmand \
+/dump/vitegourmand
+```
+
+Une fois les conteneurs démarrés, l’application sera accessible à l’adresse suivante :
+
+```txt
+http://localhost:8080
+```
+
+### Installation Clear
+
+Pour démarrer l’application avec une base de données vide, modifier la ligne 50 et 51 du fichier `docker-compose.yml` :
+
+```yaml
+# ./database/vitegourmandDBTest.sql:/docker-entrypoint-initdb.d/init.sql
+./database/vitegourmandDBClear.sql:/docker-entrypoint-initdb.d/init.sql
+```
+
+Si les conteneurs n'ont pas encore été démarrés :
+
+```bash
+docker compose up -d
+```
+
+Sinon, supprimer les volumes Docker existants avant de redémarrer :
+
+```bash
+docker compose down -v
+docker compose up -d
+```
+
+## Documentation
+
+Retrouvez l’ensemble de la documentation du projet dans le dossier `documentation` situé à la racine du dépôt.
+
+- La charte graphique de l’application
+- Les diagrammes Merise (MCD, MLD et MPD)
+- Un diagramme EER WorkBench
+- Le manuel d’utilisation avec les identifiants de démonstration (`Demo` et `Clear`)
+- Une documentation sur la gestion du projet
+- Une documentation technique
+
+## Version
+
+Version 1.0 — 17/05/2026.
+
+## Mise à jour à venir :
+
+- [ ] Amélioration du style globale
+- [ ] Ajout d'une fonctionnalité pour la création ou la suppression des allergènes
+- [ ] Ajout d'un nouveau graphique pour l'administrateur
+
+## Auteur
+
+Lucas Cappello — Projet ECF Studi
---
+++ database/vitegourmandDBClear.sql
@@ -0,0 +1,423 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for macos15 (arm64)
+--
+-- Host: 127.0.0.1    Database: vitegourmand
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `allergene`
+--
+
+DROP TABLE IF EXISTS `allergene`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `allergene` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `allergene`
+--
+
+LOCK TABLES `allergene` WRITE;
+/*!40000 ALTER TABLE `allergene` DISABLE KEYS */;
+INSERT INTO `allergene` VALUES (1,'œuf'),(2,'lait'),(3,'moutarde'),(4,'arachide'),(5,'mollusques et crustacés'),(6,'poissons'),(7,'graine de sésames'),(8,'soja'),(9,'sulfites'),(10,'noix'),(11,'blé et triticale');
+/*!40000 ALTER TABLE `allergene` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avis`
+--
+
+DROP TABLE IF EXISTS `avis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avis` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `note` int NOT NULL,
+  `libelle` varchar(50) NOT NULL,
+  `statut` varchar(50) NOT NULL,
+  `utilisateur_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UNIQ_8F91ABF0FB88E14F` (`utilisateur_id`),
+  CONSTRAINT `FK_8F91ABF0FB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `avis`
+--
+
+LOCK TABLES `avis` WRITE;
+/*!40000 ALTER TABLE `avis` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avis` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commande`
+--
+
+DROP TABLE IF EXISTS `commande`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commande` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_cmd` date NOT NULL,
+  `date_prestation` date NOT NULL,
+  `heure_livraison` time NOT NULL,
+  `prix_menu` double NOT NULL,
+  `nb_personne` int NOT NULL,
+  `prix_livraison` double NOT NULL,
+  `statut` varchar(50) NOT NULL,
+  `pret_materiel` tinyint NOT NULL,
+  `restitution_materiel` tinyint NOT NULL,
+  `utilisateur_id` int NOT NULL,
+  `menu_id` int NOT NULL,
+  `adresse_livraison` varchar(255) NOT NULL,
+  `ville_livraison` varchar(50) NOT NULL,
+  `deleted_at` date DEFAULT NULL,
+  `created_at` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_6EEAA67DFB88E14F` (`utilisateur_id`),
+  KEY `IDX_6EEAA67DCCD7E912` (`menu_id`),
+  CONSTRAINT `FK_commande_menu` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`),
+  CONSTRAINT `FK_commande_utilisateur` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commande`
+--
+
+LOCK TABLES `commande` WRITE;
+/*!40000 ALTER TABLE `commande` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commande` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `commande_historique`
+--
+
+DROP TABLE IF EXISTS `commande_historique`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `commande_historique` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `commande_id` int NOT NULL,
+  `statut` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_757DF90A82EA2E54` (`commande_id`),
+  CONSTRAINT `FK_757DF90A82EA2E54` FOREIGN KEY (`commande_id`) REFERENCES `commande` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `commande_historique`
+--
+
+LOCK TABLES `commande_historique` WRITE;
+/*!40000 ALTER TABLE `commande_historique` DISABLE KEYS */;
+/*!40000 ALTER TABLE `commande_historique` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `horaire`
+--
+
+DROP TABLE IF EXISTS `horaire`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `horaire` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `jour` varchar(50) NOT NULL,
+  `heure_ouverture` time NOT NULL,
+  `heure_fermeture` time NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `horaire`
+--
+
+LOCK TABLES `horaire` WRITE;
+/*!40000 ALTER TABLE `horaire` DISABLE KEYS */;
+INSERT INTO `horaire` VALUES (1,'Lundi','11:00:00','19:00:00'),(2,'Mardi','11:00:00','19:00:00'),(3,'Mercredi','11:00:00','19:00:00'),(4,'Jeudi','11:00:00','19:00:00'),(5,'Vendredi','11:00:00','19:00:00'),(6,'Samedi','11:00:00','19:00:00'),(7,'Dimanche','11:00:00','19:00:00');
+/*!40000 ALTER TABLE `horaire` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(50) NOT NULL,
+  `personne_mini` int NOT NULL,
+  `prix_personne` double NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `qtt_restante` int NOT NULL,
+  `created_at` date NOT NULL,
+  `deleted_at` date DEFAULT NULL,
+  `regime_id` int NOT NULL,
+  `theme_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_7D053A9335E7D534` (`regime_id`),
+  KEY `IDX_7D053A9359027487` (`theme_id`),
+  CONSTRAINT `FK_menu_regime` FOREIGN KEY (`regime_id`) REFERENCES `regime` (`id`),
+  CONSTRAINT `FK_menu_theme` FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu_plat`
+--
+
+DROP TABLE IF EXISTS `menu_plat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu_plat` (
+  `menu_id` int NOT NULL,
+  `plat_id` int NOT NULL,
+  PRIMARY KEY (`menu_id`,`plat_id`),
+  KEY `IDX_E8775249D73DB560` (`plat_id`),
+  CONSTRAINT `FK_E8775249CCD7E912` FOREIGN KEY (`menu_id`) REFERENCES `menu` (`id`),
+  CONSTRAINT `FK_E8775249D73DB560` FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu_plat`
+--
+
+LOCK TABLES `menu_plat` WRITE;
+/*!40000 ALTER TABLE `menu_plat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu_plat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `plat`
+--
+
+DROP TABLE IF EXISTS `plat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `plat` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom_plat` varchar(50) NOT NULL,
+  `photo` varchar(255) NOT NULL,
+  `deleted_at` date DEFAULT NULL,
+  `created_at` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plat`
+--
+
+LOCK TABLES `plat` WRITE;
+/*!40000 ALTER TABLE `plat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `plat_allergene`
+--
+
+DROP TABLE IF EXISTS `plat_allergene`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `plat_allergene` (
+  `plat_id` int NOT NULL,
+  `allergene_id` int NOT NULL,
+  PRIMARY KEY (`plat_id`,`allergene_id`),
+  KEY `IDX_6FA44BBF4646AB2` (`allergene_id`),
+  CONSTRAINT `FK_plat_allergene_allergene` FOREIGN KEY (`allergene_id`) REFERENCES `allergene` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_plat_allergene_plat` FOREIGN KEY (`plat_id`) REFERENCES `plat` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `plat_allergene`
+--
+
+LOCK TABLES `plat_allergene` WRITE;
+/*!40000 ALTER TABLE `plat_allergene` DISABLE KEYS */;
+/*!40000 ALTER TABLE `plat_allergene` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `regime`
+--
+
+DROP TABLE IF EXISTS `regime`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `regime` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) NOT NULL,
+  `created_at` date NOT NULL,
+  `deleted_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regime`
+--
+
+LOCK TABLES `regime` WRITE;
+/*!40000 ALTER TABLE `regime` DISABLE KEYS */;
+/*!40000 ALTER TABLE `regime` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reset_password_request`
+--
+
+DROP TABLE IF EXISTS `reset_password_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reset_password_request` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `selector` varchar(20) NOT NULL,
+  `hashed_token` varchar(100) NOT NULL,
+  `requested_at` datetime NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `utilisateur_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_7CE748AFB88E14F` (`utilisateur_id`),
+  CONSTRAINT `FK_7CE748AFB88E14F` FOREIGN KEY (`utilisateur_id`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reset_password_request`
+--
+
+LOCK TABLES `reset_password_request` WRITE;
+/*!40000 ALTER TABLE `reset_password_request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reset_password_request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_EMPLOYE'),(3,'ROLE_USER');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `theme`
+--
+
+DROP TABLE IF EXISTS `theme`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `theme` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(50) NOT NULL,
+  `created_at` date NOT NULL,
+  `deleted_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `theme`
+--
+
+LOCK TABLES `theme` WRITE;
+/*!40000 ALTER TABLE `theme` DISABLE KEYS */;
+/*!40000 ALTER TABLE `theme` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `telephone` varchar(50) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `role_id` int NOT NULL,
+  `created_at` date NOT NULL,
+  `deleted_at` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_1D1C63B3D60322AC` (`role_id`),
+  CONSTRAINT `FK_utilisateur_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+INSERT INTO `utilisateur` VALUES (1,'admin@email.com','$2y$13$6htmHjF8jtuteC3iAgB3dOwOBQWd29IWD8Kl4677WaasVVzThEBOW','adminUser','adminUser','0606060606','adminUser',1,'2026-05-01',NULL);
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-07 21:14:19
